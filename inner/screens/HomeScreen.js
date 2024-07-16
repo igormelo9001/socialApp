d54d79f55ba1193c9ac4051bcf82e562d70d0b10
unknown <igormelo9001@gmail.com>
2024-07-16 16:47:30 -0300

    fix import in homeScreen

--- inner/screens/HomeScreen.js
+++ inner/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function HomeScreen({ navigation }) {
   const posts = [
